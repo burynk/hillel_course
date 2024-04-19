2751b4faf2b47b9c6462f4ff25665f814cd0c749
Oles Buryn <40724806+burynk@users.noreply.github.com>
2024-04-19 14:22:34 +0300

    Homework 2.1

---
+++ lesson_02/task1.py
@@ -0,0 +1,8 @@
+print("Введіть ціле чотиризначне число: ")
+input_number = input()
+number = int(input_number)
+print(number//1000)
+print(number//100%10)
+print(number%100//10)
+print(number%10)
+# О - оптимізація 
