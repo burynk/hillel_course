c1ec5601f1ac3acbe60a747c4a0dff7c36999c4a
Oles Buryn <40724806+burynk@users.noreply.github.com>
2024-04-29 00:08:48 +0300

    fixed or smth

--- lesson_04/fizzbuzz.py
+++ lesson_04/fizzbuzz.py
@@ -1,12 +1,12 @@
-# ненормальне зато креативне рішення
+list1 = []
 for i in range(1, 101):
-    if i % 3 == 0:
-        print("Fizz", end="")
-        if i % 5 == 0:
-            print("Buzz")
-        else:
-            print("")
+    if i % 3 == 0 and i % 5 == 0:
+        list1.append("FizzBuzz")
+    elif i % 3 == 0:
+        list1.append("Fizz")
     elif i % 5 == 0:
-        print("Buzz")
+        list1.append("Buzz")
     else:
-        print(i)
+        list1.append(i)
+
+print(list1)
