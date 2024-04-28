eef2c58b899bec73817215cf5a55d47444453b5d
Oles Buryn <40724806+burynk@users.noreply.github.com>
2024-04-28 23:48:52 +0300

    Task 4.2 sum even elements

--- lesson_04/task2.py
+++ lesson_04/task2.py
@@ -1,6 +1,12 @@
 # list1 = [0, 1, 7, 2, 4, 8]
-list1 = [1, 3, 5]
-list2 = list1[0:-1:2]
+# list1 = [6]
+# list1 = [1, 3, 5]
+list1 = []
 
-print(list2)
-print(sum(list2) * list1[-1])
+if not list1:
+    list2 = []
+    print(list2)
+else:
+    list2 = list1[0::2]
+    print(list2)
+    print(sum(list2) * list1[-1])
