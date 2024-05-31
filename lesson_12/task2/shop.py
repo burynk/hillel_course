90b60006e8b5037e3cb19f6373bc42d53a386987
Oles Buryn <40724806+burynk@users.noreply.github.com>
2024-05-31 16:38:14 +0300

    Task 12.2

--- lesson_12/task2/shop.py
+++ lesson_12/task2/shop.py
@@ -8,13 +8,15 @@ class Item:
     Represents an item in the shop
     """
 
-    def __init__(self, name, price, description, dimensions):
+    def __init__(
+        self, name: str, price: float, description: str, dimensions: str
+    ) -> None:
         self.price = price
         self.description = description
         self.dimensions = dimensions
         self.name = name
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name + ", price: " + str(self.price)
 
 
@@ -23,12 +25,12 @@ class User:
     Represents a user in the shop
     """
 
-    def __init__(self, name, surname, numberphone):
+    def __init__(self, name: str, surname: str, numberphone: str) -> None:
         self.name = name
         self.surname = surname
         self.numberphone = numberphone
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name + " " + self.surname
 
 
@@ -37,12 +39,12 @@ class Purchase:
     Represents a purchase in the shop
     """
 
-    def __init__(self, user):
+    def __init__(self, user: User) -> None:
         self.products = {}
         self.user = user
         self.total = 0
 
-    def add_item(self, item, cnt):
+    def add_item(self, item: Item, cnt: int) -> None:
         """
         Change the amount of items in the purchase
         :param item: The item itself
@@ -50,13 +52,13 @@ class Purchase:
         """
         self.products[item] = cnt
 
-    def __str__(self):
+    def __str__(self) -> str:
         items_str = "\n".join(
             [f"{item.name}: {cnt} pcs." for item, cnt in self.products.items()]
         )
         return f"User: {self.user.name} {self.user.surname}\n" f"Items:\n{items_str}"
 
-    def get_total(self):
+    def get_total(self) -> float:
         """
         Calculates the total cost of purchase
         :return: The total cost
